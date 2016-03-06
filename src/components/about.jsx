ea8d635968553accb345f491e2a80ca6b616b72b
PiggySpeed <jlee14c@hotmail.com>
2016-03-05 16:36:53 -0800

    working on rightcol

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,6 +4,12 @@ import MiddleCol from './middlecol.jsx';
 import RightCol from './rightcol.jsx';
 
 export default class AboutPage extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      data: ["hallo card 1", "hallo card 2"]
+    }
+  }
   render() {
     return(
       <div>
--- src/components/rightcol.jsx
+++ src/components/rightcol.jsx
@@ -4,76 +4,51 @@ import Paper from '../../node_modules/material-ui/lib/paper';
 //import Slider from '../../node_modules/material-ui/lib/slider';
 import Card from '../../node_modules/material-ui/lib/card/card';
 import RaisedButton from '../../node_modules/material-ui/lib/raised-button';
-//import Colors from '../../node_modules/material-ui/lib/styles/colors';
+import Colors from '../../node_modules/material-ui/lib/styles/colors';
 //import LeftNav from '../../node_modules/material-ui/lib/left-nav';
 import MenuItem from '../../node_modules/material-ui/lib/menus/menu-item';
 import TextField from '../../node_modules/material-ui/lib/text-field';
 
-var data = [
-  {card1: "THIS IS CARD1"},
-  {card2: "THIS IS CARD2!"}
-];
 
-class ShortPaper extends React.Component {
+class WeeklyList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      style: {
-        width: 375,
-        height: 50,
-        position: "relative",
-        top: -24,
-        margin: 6
-      },
-      data: {
-        input: ""
-      }
-    };
-  };
-  displayContents() {
-    return (
-      this.state.data.input
-    )
-  };
+  }
   render() {
     return (
-      <Paper onClick={this.props.onPiggy} style={this.state.style}>
-        {this.props.results}
-      </Paper>
-   )
-  };
+      <Card className="weeklyListCard"><p>{this.props.foogle}</p></Card>
+    )
+  }
+}
+class MonthlyList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Card className="monthlyListCard"><p>{this.props.addItem}</p></Card>
+    )
+  }
 }
 
 export default class RightCol extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      cards: [],
-      text: "its working now"
+      listItems: ["cow", "pig"]
     };
-    this.generateList();
-  };
-  generateList() {
-    for (var i=0; i<data.length; i++) {
-      this.state.cards.push(data[i]);
-    }
-  }
-  piggyTest(){
-    this.setState({
-      text: "I CHANGED THE STATE"
-    })
-  }
-  piggyTest2() {
-    this.setState({
-      text: "BACK TO NORMAL!!"
-    })
   }
   render() {
     return (
-      <div>
-        <RaisedButton onClick={this.piggyTest.bind(this)}>ClickMe</RaisedButton>
-        <ShortPaper onPiggy={this.piggyTest2.bind(this)} results={this.state.text}/>
+      <div className="right-col-container">
+        <WeeklyList className="weeklyList" foogle="fecgffly"/>
+        <MonthlyList addItem="piggly"/>
       </div>
     )
   }
 }
+
+var styles = {
+  background: {
+    backgroundColor: "#00ee00"}
+};
